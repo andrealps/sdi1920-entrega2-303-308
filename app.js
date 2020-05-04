95e186306cee7593e2c189ecbfddb6f2f6fd9709
Sara <uo258060@uniovi.es>
2020-05-05 01:16:12 +0200

    Identificarse con usuario-token

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 let express = require('express');
 let app = express();
 
+// Token
+var jwt = require('jsonwebtoken');
+app.set('jwt', jwt);
+
 // Seguridad
 let fs = require('fs');
 let https = require('https');
@@ -27,23 +31,49 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // Gestor de la BD
 let gestorBD = require("./modules/gestorBD.js");
-gestorBD.init(app,mongo);
+gestorBD.init(app, mongo);
+
+// routerUsuarioToken
+var routerUsuarioToken = express.Router();
+routerUsuarioToken.use(function (req, res, next) {
+    var token = req.headers['token'] || req.body.token || req.query.token;
+    if (token != null) {
+        // verificar el token
+        jwt.verify(token, 'secreto', function (err, infoToken) {
+            if (err || (Date.now() / 1000 - infoToken.tiempo) > 240) {
+                res.status(403); // Forbidden
+                res.json({acceso: false, error: 'Token invalido o caducado'});
+                return;
+            } else {
+                // dejamos correr la petición
+                res.usuario = infoToken.usuario;
+                next();
+            }
+        });
+    } else {
+        res.status(403); // Forbidden
+        res.json({acceso: false, mensaje: 'No hay Token'});
+    }
+});
+
+// Aplicar routerUsuarioToken
+app.use('/api/usuario', routerUsuarioToken);
 
 // routerUsuarioSession
 let routerUsuarioSession = express.Router();
-routerUsuarioSession.use(function(req, res, next) {
+routerUsuarioSession.use(function (req, res, next) {
     console.log("routerUsuarioSession");
-    if ( req.session.usuario ) {
+    if (req.session.usuario) {
         // dejamos correr la petición
         next();
     } else {
-        console.log("va a : "+req.session.destino);
+        console.log("va a : " + req.session.destino);
         res.redirect("/identificarse");
     }
 });
 
 //Aplicar routerUsuarioSession
-app.use("/listaUsuarios",routerUsuarioSession);
+app.use("/listaUsuarios", routerUsuarioSession);
 
 // Declaración del directorio public como estático
 app.use(express.static('public'));
@@ -51,13 +81,14 @@ app.use(express.static('public'));
 // Variables
 app.set('port', 8081);
 app.set('db', 'mongodb://admin:sdi@mysocialnetwork-shard-00-00-7kwvz.mongodb.net:27017,mysocialnetwork-shard-00-01-7kwvz.mongodb.net:27017,mysocialnetwork-shard-00-02-7kwvz.mongodb.net:27017/test?ssl=true&replicaSet=MySocialNetwork-shard-0&authSource=admin&retryWrites=true&w=majority');
-app.set('clave','abcdefg');
-app.set('crypto',crypto);
+app.set('clave', 'abcdefg');
+app.set('crypto', crypto);
 
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig, gestorBD);
 require("./routes/rlogUsuarios")(app, swig, gestorBD);
 require("./routes/rdatosPruebas")(app, swig, gestorBD);
+require("./routes/rapiLogUsuarios.js")(app, gestorBD);
 
 app.get('/', function (req, res) {
     res.redirect('/listaUsuarios');
@@ -73,7 +104,7 @@ https.createServer({
 
 // Renderiza las vistas pasándoles el usuario en sesión
 app.renderView = (view, session, respuesta) => {
-    respuesta = respuesta ? respuesta: {};
+    respuesta = respuesta ? respuesta : {};
     respuesta.usuario = session.usuario;
     return swig.renderFile(view, respuesta);
 };
---
+++ routes/rapiLogUsuarios.js
@@ -0,0 +1,44 @@
+module.exports = function (app, gestorBD) {
+
+    app.get("/api/usuario", function (req, res) {
+        gestorBD.obtenerUsuarios({}, function (usuarios) {
+            if (usuarios == null) {
+                res.status(500);
+                res.json({error: "se ha producido un error"})
+            } else {
+                res.status(200);
+                res.send(JSON.stringify(usuarios));
+            }
+        });
+    });
+
+    /**
+     * S1 - Identificarse con usuario – token
+     */
+    app.post("/api/autenticar/", function (req, res) {
+        let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
+
+        let criterio = {
+            email: req.body.email,
+            password: seguro
+        };
+
+        gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+            if (usuarios == null || usuarios.length === 0) {
+                res.status(401);
+                res.json({
+                    autenticado: false
+                })
+            } else {
+                var token = app.get('jwt').sign({usuario: criterio.email, tiempo: Date.now() / 1000}, "secreto");
+                res.status(200);
+                res.json({
+                    autenticado: true,
+                    token : token
+                })
+            }
+        });
+
+    });
+}
